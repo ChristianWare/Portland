464919c6d6c22bd969072388de5836f83a6c9975
Chris <chris.ware.dev@gmail.com>
2021-10-25 14:07:49 -0700

    Hero/Skills Section Updates

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/Contact.modules.css'
+
+function Contact() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Contact
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,18 +6,29 @@ function Skills() {
     <div className={styles.aContainer}>
       <div className={styles.aRight}>
         <h5 className={styles.aSub}>Front End</h5>
-        <p className={styles.aDescription}>HTML, CSS (Styled-Components, SASS, CSS Modules, Bootstrap, Material-UI), JavaScript (ES6)</p>
+        <p className={styles.aDescription}>HTML, CSS, JavaScript (ES6)</p>
         <h5 className={styles.aSub}>Back End</h5>
         <p className={styles.aDescription}>
           NodeJS, Express, Mongo Atlas, Strapi, Contentful
         </p>
-        <h5 className={styles.aSub}>Frame Works</h5>
-        <p className={styles.aDescription}>NextJS, React</p>
+        <h5 className={styles.aSub}>Libraries & Frame Works</h5>
+        <p className={styles.aDescription}>
+          NextJS, React, Styled-Components, SASS, Bootstrap, Material-UI, Framer
+          Motion, GSAP, Three.JS
+        </p>
         <h5 className={styles.aSub}>Design Tools</h5>
-        <p className={styles.aDescription}>Figma, Balsalmic, Photoshop, Illustrator</p>
+        <p className={styles.aDescription}>
+          Figma, Balsalmic, Photoshop, Illustrator
+        </p>
+        <h5 className={styles.aSub}>Miscellaneous</h5>
+        <p className={styles.aDescription}>
+          VS Code, Atom, Terminal/Command Line, Chrome Developer Tools, Version Control
+        </p>
       </div>
       <div className={styles.aLeft}>
         <div className={styles.cubeContainer}>
+          <Cube>Skills</Cube>
+          <Cube>Skills</Cube>
           <Cube>Skills</Cube>
         </div>
       </div>
