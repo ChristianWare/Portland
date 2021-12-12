0fddef278db0651e3ba1ff3390725a12f446599d
Chris <chris.ware.dev@gmail.com>
2021-12-12 02:03:30 -0700

    Image Alighnment Update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,33 +14,36 @@ function Projects() {
           <div className={styles.item}>
             <div className={styles.right}>
               <h2 className={styles.projectTitle}>{project.title}</h2>
-              <p lang='en' className={styles.projectDescription}>
-                {project.description}
-              </p>
-              <p lang='en' className={styles.projectDescription}>
-                {project.tech}
-              </p>
-              <h3>Preview :</h3>
-              <div className={styles.imgContainer}>
-                <Image
-                  alt='preview'
-                  src={project.image}
-                  width={project.width}
-                  height={project.height}
-                />
+              <div className={styles.descLeft}>
+                <div className={styles.text}>
+                  <p lang='en' className={styles.projectDescription}>
+                    {project.description}
+                  </p>
+                  <p lang='en' className={styles.projectDescription}>
+                  {project.tech}
+                </p>
+                </div>
+                <div className={styles.imgContainer}>
+                  <Image
+                    alt='preview'
+                    src={project.image}
+                    width={project.width}
+                    height={project.height}
+                  />
+                </div>
               </div>
-              <div className={styles.buttonContainer}>
-                <Link href={project.github}>
-                  <a className={styles.button1} target='_blank'>
-                    GitHub
-                  </a>
-                </Link>
-                <Link href={project.url}>
-                  <a className={styles.button2} target='_blank'>
-                    Live Site
-                  </a>
-                </Link>
-              </div>
+                  <div className={styles.buttonContainer}>
+                    <Link href={project.github}>
+                      <a className={styles.button1} target='_blank'>
+                        GitHub
+                      </a>
+                    </Link>
+                    <Link href={project.url}>
+                      <a className={styles.button2} target='_blank'>
+                        Live Site
+                      </a>
+                    </Link>
+                  </div>
             </div>
             <div className={styles.buttonContainer}></div>
           </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -24,20 +24,20 @@ const projects = [
     github: "https://github.com/ChristianWare/Periodic-Table-Of-Telements",
     url: "https://telements.co/",
     image: img2,
-    width: 700,
-    height: 583,
+    width: 500,
+    height: 413,
   },
   {
     id: 3,
     title: "Taco Bell",
     description:
-      "This website is a redesign of Taco Bell's web page. I thought their website could use a refresh so I took it upon myself to take on this challenge. I wanted the main focus on the site to be the menu, since the menu on Taco Bell's website is not currently on their home page (you have to navigate to another page to see it). The menu I created is able to be filtered to show breakfast, lunch, and all items on the menu at the same time. The Site is mobile responsive, and received high scores on Google's Page Speed Insights. There is also a brief 'about' section, a social media section with links to external pages, and a newsletter sign up area towards the bottom. Feel free to visit the demo link above.",
+      "This website is a redesign of Taco Bell's web page. I thought their website could use a refresh so I took it upon myself to take on this challenge. I wanted the main focus on the site to be the menu so that it is able to be filtered to show breakfast, lunch, and all items on the menu at the same time. The Site is mobile responsive, and received high scores on Google's Page Speed Insights.",
     tech: "This Single Page Application was built with Next JS and React. The Data for the menu items is being pulled from a headless CMS, Contentful, with the 'getStaticProps' function in NextJs. It is styled using CSS Modules. The dependencies used were react-icons, and the Contentful CLI. The API keys were stored in a .env.local file, to be hidden from the public. The images were optimized for production, and are lazy loaded on the page. This is all possible because of the Image component from Next JS.",
     github: "https://github.com/ChristianWare/Taco-Bell",
     url: "https://livemas.io/",
     image: img3,
-    width: 700,
-    height: 608,
+    width: 510,
+    height: 438,
   },
 ];
 
