684f24e83f370a7120c0cbe88b0127f3a430ece0
Chris <chris.ware.dev@gmail.com>
2022-03-25 01:36:22 -0700

    Skills/Favicon Update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,20 +9,20 @@ function Skills() {
           01. <br /> <br />
           Skills & Technologies
         </h3>
-        <h3 className={styles.heading}>Front End</h3>
+        <h5 className={styles.heading}>Front End</h5>
         <p className={styles.copy}>HTML, CSS, JavaScript (ES6)</p>
-        <h3 className={styles.heading}>Back End</h3>
+        <h5 className={styles.heading}>Back End</h5>
         <p className={styles.copy}>
           NodeJS/npm, Express, Mongo Atlas, Firebase, Strapi, Contentful
         </p>
-        <h3 className={styles.heading}>Libraries & Frame Works</h3>
+        <h5 className={styles.heading}>Libraries & Frame Works</h5>
         <p className={styles.copy}>
           NextJS, React, Styled-Components, SASS, Bootstrap, Material-UI, Framer
           Motion, GSAP, Three.JS
         </p>
-        <h3 className={styles.heading}>Design Tools</h3>
+        <h5 className={styles.heading}>Design Tools</h5>
         <p className={styles.copy}>Figma, Balsamiq, Photoshop, Illustrator</p>
-        <h3 className={styles.heading}>Miscellaneous</h3>
+        <h5 className={styles.heading}>Miscellaneous</h5>
         <p className={styles.copy}>
           VS Code, Atom, Terminal/Command Line, Chrome Developer Tools, Version
           Control(Git), Markdown, Postman, JSON, REST API&apos;s, Slack, Zoom
