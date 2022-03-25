b422c845cff9660b8f95c37ed2ed095de7beda50
Chris <chris.ware.dev@gmail.com>
2022-03-24 23:40:41 -0700

    Footeer/Button Fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,106 +1,83 @@
 import Link from "next/link";
-import { SiHackthebox } from "react-icons/si";
-import { BsGithub, BsLinkedin } from "react-icons/bs";
-import { AiOutlineMail } from "react-icons/ai";
 import styles from "../../styles/Footer.module.css";
+import { FaCube } from "react-icons/fa";
+import { GrFacebookOption } from "react-icons/gr";
+import { BsInstagram } from "react-icons/bs";
+import { FiYoutube } from "react-icons/fi";
 
 function Footer() {
   return (
-    <footer className={styles.fContainer}>
-      <div className={styles.row}>
-        <div className={styles.col}>
-          <div className={styles.logoContainer}>
-            <Link href='/'>
-              <span className={styles.fLogo}>
-                <SiHackthebox className={styles.fIcon} />
+    <section className={styles.footerContainer}>
+      <footer className={styles.footerBottom}>
+        <div className={styles.footerContent}>
+          <div className={styles.footerLeft}>
+            <Link href='/' passHref>
+              <span className={styles.navbarLogo}>
+                <FaCube className={styles.navbarIcon} />
                 <a>Christian Ware</a>
               </span>
             </Link>
+            <p lang='en'>
+              Expand on the keyword with more keywords and add a list of related
+              services that you do and include the city, state of your service
+              area.
+            </p>
+            <div className={styles.sci}>
+              <GrFacebookOption className={styles.icon} />
+              <BsInstagram className={styles.icon} />
+              <FiYoutube className={styles.icon3} />
+            </div>
           </div>
-
-          <p className={styles.fDescription}>
-            Thank you for visiting my personal portfolio website. Feel free to
-            reach out to me for more information.
-          </p>
-          <Link href='/'>
-            <a className={styles.btt}>Back to Top</a>
-          </Link>
+          <div className={styles.footerRight}>
+            <div className={styles.list1}>
+              <h6 className={styles.heading}>Skills</h6>
+              <ul>
+                <Link href='/' passHref>
+                  <li>Beards</li>
+                </Link>
+                <li>Mustashes</li>
+                <li>Hair Cuts</li>
+                <li>Kids</li>
+              </ul>
+            </div>
+            <div className={styles.list2}>
+              <h6 className={styles.heading}>About</h6>
+              <ul>
+                <Link href='/' passHref>
+                  <li>Our Story</li>
+                </Link>
+                <li>Barbers</li>
+                <li>Location</li>
+                <li>Prices</li>
+              </ul>
+            </div>
+            <div className={styles.list3}>
+              <h6 className={styles.heading}>Projects</h6>
+              <ul>
+                <Link href='/' passHref>
+                  <li>Address</li>
+                </Link>
+                <li>Email</li>
+                <li>Phone</li>
+                <li>Insta</li>
+              </ul>
+            </div>
+          </div>
         </div>
-        <div className={styles.col}>
-          <Link href='#contact' passHref>
-            <a>
-              <h5>Contact</h5>
-            </a>
-          </Link>
-          <p>602-842-4048</p>
-          <a href='mailto:chris.ware.dev@gmail.com'>chris.ware.dev@gmail.com</a>
-          <div className={styles.fSocialIcons}>
-            <Link href='https://github.com/ChristianWare'>
-              <a target='_blank'>
-                <BsGithub className={styles.fSocialIcon} />
+        <hr className={styles.hr} />
+        <div className={styles.copyrightContainer}>
+          <small>
+            Christian Ware {new Date().getFullYear()} &copy; - All Rights
+            Reserved || Designed/Developed by{" "}
+            <Link href='https://chrisware.io' passHref>
+              <a className={styles.cw} target='_blank'>
+                Chris Ware
               </a>
             </Link>
-            <Link href='https://www.linkedin.com/in/christian-ware/' passHref>
-              <a target='_blank'>
-                <BsLinkedin className={styles.fIcon} />
-              </a>
-            </Link>
-          </div>
+          </small>
         </div>
-        <div className={styles.col}>
-          <Link href='#projects' passHref>
-            <a>
-              <h5>Projects</h5>
-            </a>
-          </Link>
-          <ul>
-            <li>
-              <a>Portland</a>
-            </li>
-
-            <li>
-              <a>Taco Bell</a>
-            </li>
-
-            <li>
-              <a>Telements</a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.col}>
-          <Link href='#skills'>
-            <a>
-              <h5>Skills</h5>
-            </a>
-          </Link>
-          <ul>
-            <li>
-              <a>React</a>
-            </li>
-
-            <li>
-              <a>NextJS</a>
-            </li>
-
-            <li>
-              <a>JavaScript</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <hr className={styles.bottomBorder} />
-      <div className={styles.copyContainer}>
-        <p className={styles.fCopy}>
-          <span className={styles.fCopyLogo}>
-            <SiHackthebox className={styles.fcopyIcon} />
-            <a>
-              Christian Ware {new Date().getFullYear()} &copy; - All Rights
-              Reserved
-            </a>
-          </span>
-        </p>
-      </div>
-    </footer>
+      </footer>
+    </section>
   );
 }
 
