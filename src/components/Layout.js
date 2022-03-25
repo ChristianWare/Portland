a16405b4482feecd2f5a7786155f1b8f23ce1e33
Chris <chris.ware.dev@gmail.com>
2022-03-25 01:01:26 -0700

    Scroll To Top

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,16 @@
-import styles from '../../styles/Layout.module.css'
+import styles from "../../styles/Layout.module.css";
 import Footer from "./Footer";
-import Navbar from './Navbar';
-import ScrollInicator from './ScrollIndicator'
+import Navbar from "./Navbar";
+import ScrollInicator from "./ScrollIndicator";
+import ScrollToTop from "./ScrollToTop";
 
 function Layout({ children }) {
   return (
     <div className={styles.container}>
       <div className={styles.background}>
         <div className={styles.layout}>
-        <ScrollInicator />
-        
+          <ScrollInicator />
+          <ScrollToTop />
           <Navbar />
           {children}
           <Footer />
