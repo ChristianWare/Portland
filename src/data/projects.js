20d24ba922a3647fc2917ca398a2c2ef69bf8cd6
Chris <chris.ware.dev@gmail.com>
2022-03-25 16:06:57 -0700

    Data Spell Correction

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,7 +10,7 @@ const projects = [
     title: "Taco Bell",
     description:
       "This website is a redesign of Taco Bell's web page. I wanted the main focus on the site to be the menu so that it is able to be filtered to show breakfast, lunch, and all items on the menu at the same time. The Site is mobile responsive, and received high scores on Google's Page Speed Insights.",
-    tech: "Next JS/React, CMS Contentful, CSS Modules, React-icons.",
+    tech: "Next JS/React, CMS Contentful, CSS Modules, React-Icons.",
     github: "https://github.com/ChristianWare/Taco-Bell",
     url: "https://livemas.io/",
     image: livemas,
