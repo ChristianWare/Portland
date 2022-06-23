ce8287a7df3af34aefb25416cac8a7cb1d1d1b87
Chris Ware <c.p.ware@hotmail.com>
2022-06-22 21:07:07 -0700

    Circle Text Component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -30,41 +30,7 @@ function Hero() {
                   I build sematically structured web applications by thinking
                   outside the div.
                 </h2>
-                {/* <svg
-                  className={styles.svg}
-                  viewBox='0 0 100 100'
-                  width='250'
-                  height='250'
-                >
-                  <defs>
-                    <path
-                      id='circle'
-                      d='
-        M 50, 50
-        m -37, 0
-        a 37,37 0 1,1 74,0
-        a 37,37 0 1,1 -74,0'
-                    />
-                    <animateTransform
-                      attributeName='transform'
-                      begin='0s'
-                      dur='30s'
-                      type='rotate'
-                      from='0 250 250'
-                      to='360 250 250'
-                      repeatCount='indefinite'
-                    />
-                  </defs>
-                  <text font-size='11.5' font-family='monospace'>
-                    <textPath
-                      className={styles.svg}
-                      xlinkHref='#circle'
-                      font-family='monospace'
-                    >
-                      You spin me right round, baby.
-                    </textPath>
-                  </text>
-                </svg> */}
+                
               </div>
               <div className={styles.buttonContainer}>
                 <Link href='#projects'>
