76dbc1e172fca5b518b04e06f6f0ae2965c89bf5
ChristianWare <c.p.ware@hotmail.com>
2022-07-23 01:08:15 -0700

    New Drone Image

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,5 @@
 import telements from "../../public/images/telements.png";
-import razor from "../../public/images/razor.png";
+import drone from "../../public/images/drone.png";
 import livemas from "../../public/images/livemas.png";
 import portland from "../../public/images/portland.png";
 import fonts from "../../public/images/fonts.png";
@@ -20,27 +20,13 @@ const projects = [
   },
   {
     id: 2,
-    title: "Taco Bell",
+    title: "Dronopoly",
     description:
-      "This website is a redesign of Taco Bell's web page. I wanted the main focus on the site to be the menu so that it is able to be filtered to show breakfast, lunch, and all items on the menu at the same time. The Site is mobile responsive, and received high scores on Google's Page Speed Insights.",
-    tech: "Next JS/React, CMS Contentful, CSS Modules, React-Icons",
-    github: "https://github.com/ChristianWare/Taco-Bell",
-    url: "https://livemas.io/",
-    image: livemas,
-    frontEnd: "React/Next.js",
-    backEnd: "Node.js",
-    npmDep: "emailjs-com, nodemailer, react-icons",
-    styling: "CSS Modules",
-  },
-  {
-    id: 3,
-    title: "Razor Edge",
-    description:
-      "My personal barber recently expressed to me that he does not have a website, so I offered to build him one. When he accepted, I gave him a few designs, and this is the one that he chose. In addition to the home page, it has a gallery, an about page, and service details page. It is mobile responsive, and and received high scores on Google's Page Speed Insights.",
+      "This is a fictional drone company that I created. They specialize in drone photography and videography. It currently has a home services, about, gallery, and contact page that can all be easily accessed on the navbar. There is a contact form that is fully functional. The site is mobile friendly and recieived high scores on Google page speed insights. ",
     tech: "Next JS/React, CSS Modules, React-Icons, Framer Motion",
-    github: "https://github.com/ChristianWare/Razor-Edge",
-    url: "https://www.razoredgebarber.com/",
-    image: razor,
+    github: "https://github.com/ChristianWare/templateOne",
+    url: "https://dronopoly.com/",
+    image: drone,
     frontEnd: "React/Next.js",
     backEnd: "Node.js",
     npmDep: "emailjs-com, nodemailer, react-icons",
@@ -55,6 +41,20 @@ const projects = [
     github: "https://github.com/ChristianWare/Periodic-Table-Of-Telements",
     url: "https://telements.co/",
     image: telements,
+    frontEnd: "React/Next.js",
+    backEnd: "Node.js",
+    npmDep: "emailjs-com, nodemailer, react-icons",
+    styling: "CSS Modules",
+  },
+  {
+    id: 3,
+    title: "Taco Bell",
+    description:
+      "This website is a redesign of Taco Bell's web page. I wanted the main focus on the site to be the menu so that it is able to be filtered to show breakfast, lunch, and all items on the menu at the same time. The Site is mobile responsive, and received high scores on Google's Page Speed Insights.",
+    tech: "Next JS/React, CMS Contentful, CSS Modules, React-Icons",
+    github: "https://github.com/ChristianWare/Taco-Bell",
+    url: "https://livemas.io/",
+    image: livemas,
     frontEnd: "React/Next.js",
     backEnd: "Node.js",
     npmDep: "emailjs-com, nodemailer, react-icons",
