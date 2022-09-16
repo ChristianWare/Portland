004827f277dbcdce41799d7540a473a8014de77d
Chris Ware <c.p.ware@hotmail.com>
2022-09-16 01:17:39 -0700

    Added Phone and Email to Time

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { GoLocation } from 'react-icons/go';
+import { BsTelephone } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+
 import styles from '../../styles/Time.module.css'
 
 function Time() {
@@ -11,16 +14,22 @@ function Time() {
 
     return (
       <div className={styles.timeContainer}>
-          <p className={styles.clock}>
-            <GoLocation className={styles.icon} />
-            {date.toLocaleString("en-US", {
-              timeZone: "America/Phoenix",
-              hour: "numeric",
-              minute: "numeric",
-              hour12: true,
-            })}
-            _MST
-          </p>
+        <p className={styles.clock}>
+          <GoLocation className={styles.icon} />
+          {date.toLocaleString("en-US", {
+            timeZone: "America/Phoenix",
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+          })}
+          : Phoenix, AZ
+        </p>
+        <a href='tel:623-335-52514' className={styles.clock}>
+          <BsTelephone /> 480-808-2054
+        </a>
+        <a href='mailto:chris.ware.dev@gmail.com' className={styles.clock}>
+          <AiOutlineMail className={styles.icon2} /> chris.ware.dev@gmail.com
+        </a>
       </div>
     );
 }
