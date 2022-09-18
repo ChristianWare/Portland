9229497425aba789e643913d200b385c84a50ea6
Chris Ware <c.p.ware@hotmail.com>
2022-09-17 17:20:37 -0700

    Project Image Border Radius

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,51 +8,62 @@ function Projects() {
   return (
     <section className={styles.projects}>
       <div className={styles.headingTop}>
-        <h3 className={styles.heading} lang="en" id="projects">
+        <h3 className={styles.heading} lang='en' id='projects'>
           <span>02.</span> <br />
           Projects & Details
         </h3>
         <div className={styles.cubeContainer}>
-          <ProjectsCube className={styles.cubeContainer} text='Projects'/>
+          <ProjectsCube className={styles.cubeContainer} text='Projects' />
         </div>
       </div>
 
       {projects.map(
-        ({ id, title, description, frontEnd, backEnd, npmDep, styling, image, github, url }) => (
+        ({
+          id,
+          title,
+          description,
+          frontEnd,
+          backEnd,
+          npmDep,
+          styling,
+          image,
+          github,
+          url,
+        }) => (
           <div key={id} className={styles.allItems}>
             <div className={styles.item}>
               <div className={styles.right}>
                 <div className={styles.descLeft}>
                   <div className={styles.text}>
                     <h2 className={styles.projectTitle}>{title}</h2>
-                    <p lang="en" className={styles.projectDescription}>
+                    <p lang='en' className={styles.projectDescription}>
                       {description}
                     </p>
-                    <p lang="en" className={styles.tech}>
+                    <p lang='en' className={styles.tech}>
                       Front End: <span>{frontEnd}</span>
                     </p>
-                    <p lang="en" className={styles.tech}>
+                    <p lang='en' className={styles.tech}>
                       Back End: <span>{backEnd}</span>
                     </p>
-                    <p lang="en" className={styles.tech}>
+                    <p lang='en' className={styles.tech}>
                       NPM Dependencies: <span>{npmDep}</span>
                     </p>
-                    <p lang="en" className={styles.tech}>
+                    <p lang='en' className={styles.tech}>
                       Styling: <span>{styling}</span>
                     </p>
                   </div>
                   <div className={styles.imgContainer}>
-                    <Image alt="preview" src={image} className={styles.img} />
+                    <Image alt='preview' src={image} className={styles.img} />
                   </div>
                 </div>
                 <div className={styles.buttonContainer}>
                   <Link href={github}>
-                    <a className={styles.button1} target="_blank">
+                    <a className={styles.button1} target='_blank'>
                       GitHub
                     </a>
                   </Link>
                   <Link href={url}>
-                    <a className={styles.button2} target="_blank">
+                    <a className={styles.button2} target='_blank'>
                       Live Site
                     </a>
                   </Link>
