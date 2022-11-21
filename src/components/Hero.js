3bf2db7148e4caba25e48a9ec7624fabae4274f8
Chris Ware <c.p.ware@hotmail.com>
2022-11-21 01:49:31 -0700

    Spelling Error

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,23 +4,23 @@ import Cube from "../components/Cubes/Cube";
 
 function Hero() {
   return (
-    <header id="#home" className={styles.hero}>
+    <header id='#home' className={styles.hero}>
       <div className={styles.heroLeft}>
         <div className={styles.heroLeftWrapper}>
           <h1 className={styles.heroName}>Hello There. My Name is Chris.</h1>
           <div className={styles.heroTitle}>
             <div className={styles.titleWrapper}>
               <div className={styles.titleItem}>
-                <p className={styles.copy} lang="en">
-                  I build sematically structured web applications by thinking
+                <p className={styles.copy} lang='en'>
+                  I build semantically structured web applications by thinking
                   outside the div.
                 </p>
               </div>
               <div className={styles.buttonContainer}>
-                <Link href="#projects" passHref>
+                <Link href='#projects' passHref>
                   <a className={styles.button1}>My Projects</a>
                 </Link>
-                <Link href="#skills" passHref>
+                <Link href='#skills' passHref>
                   <a className={styles.button2}>My Skills</a>
                 </Link>
               </div>
@@ -30,7 +30,7 @@ function Hero() {
       </div>
       <div className={styles.heroRight}>
         <div className={styles.cubeContainer}>
-          <Cube text="div" />
+          <Cube text='div' />
         </div>
       </div>
     </header>
